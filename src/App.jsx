fada5a247a7f3cc0a27d8d571ba4291afda87976
Achfa Wade Ruapensa <achfa.ruapensa@toyota.co.id>
2024-08-09 16:11:35 +0700

    React.js

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home';
+import Cari from './pages/Cari';
+import Detail from './pages/Detail';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cari" element={<Cari />} />
+        <Route path="/cari/detail/:id" element={<Detail />} />
+
+      </Routes>
+    </>
+  )
+}
+
+export default App
